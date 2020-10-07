b7e1f81171bf55666712160cd3bce215ea8c8ed4
Soon9 <naong526@naver.com>
2020-10-07 16:40:56 +0900

    [20.10.07] # Initial Commit
    Signed-off-by: Soon9 <naong526@naver.com>

--- src/App_before.js
+++ src/App_before.js
@@ -1,5 +1,4 @@
 import React, {useCallback, useRef, useState} from 'react';
-import produce from "immer";
 
 const App = () => {
 
@@ -14,12 +13,10 @@ const App = () => {
     const onChange = useCallback(
         e => {
             const {name, value} = e.target;
-
-            setForm(
-              produce(form, draft => {
-                  draft[name] = value;
-              })
-            );
+            setForm({
+                ...form,
+                [name]: [value]
+            });
         }, [form]
     );
 
@@ -34,11 +31,10 @@ const App = () => {
             };
 
             //array에 새 항목 등록
-            setData(
-                produce(data, draft => {
-                    draft.array.push(info)
-                })
-            );
+            setData({
+                ...data,
+                array: data.array.concat(info)
+            });
 
             //form 초기화
             setForm({
@@ -52,11 +48,10 @@ const App = () => {
     //항목을 삭제하는 함수
     const onRemove = useCallback(
         id => {
-            setData(
-                produce(data, draft => {
-                    draft.array.splice(draft.array.findIndex(info => info.id === id), 1);
-                })
-            );
+            setData({
+                ...data,
+                array: data.array.filter(info => info.id !== id)
+            });
         }, [data]
     );
 
